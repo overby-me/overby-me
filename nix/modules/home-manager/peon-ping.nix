c2b0581178ba0be67fefe2df98d3770a0e30c0c4
Niclas Overby <niclas@overby.me>
2026-03-19 16:43:37 +0100

    feat(nix/modules): Add WC3/Warcraft sound packs with random rotation for peon-ping
    Signed-off-by: Niclas Overby <niclas@overby.me>

--- nix/modules/home-manager/peon-ping.nix
+++ nix/modules/home-manager/peon-ping.nix
@@ -65,7 +65,89 @@ in {
       volume = 0.5;
       enabled = true;
       desktop_notifications = true;
+      pack_rotation = [
+        "peon"
+        "murloc"
+        "peasant"
+        "wc2_peasant"
+        "wc2_human_ships"
+        "wc2_sapper"
+        "wc3_brewmaster"
+        "wc3_corrupted_arthas"
+        "wc3_farseer"
+        "wc3_grunt"
+        "wc3_jaina"
+        "wc3_knight"
+        "warcraft-peon"
+        "wow-tauren"
+      ];
+      pack_rotation_mode = "random";
     };
-    installPacks = ["peon"];
+    installPacks = [
+      # og-packs (bundled)
+      "peon"
+      "murloc"
+      "peasant"
+      "wc2_peasant"
+      "wc3_brewmaster"
+      "wc3_farseer"
+      "wc3_grunt"
+      "wc3_knight"
+      # Community packs
+      {
+        name = "warcraft-peon";
+        src = pkgs.fetchFromGitHub {
+          owner = "rocktane";
+          repo = "openpeon-warcraft-peon";
+          tag = "v1.0.0";
+          hash = "sha256-6qs/JbuJ167tnGiTxwx2Di8OHpow7svFLhcq5aYetq0=";
+        };
+      }
+      {
+        name = "wc2_human_ships";
+        src = pkgs.fetchFromGitHub {
+          owner = "dmnd";
+          repo = "peonping-wc2_human_ships";
+          tag = "v1.0.0";
+          hash = "sha256-yxxKfNUTtP3adNPJBCkgg2r+GXrKUVdj2ZcPCx2rYGs=";
+        };
+      }
+      {
+        name = "wc2_sapper";
+        src = pkgs.fetchFromGitHub {
+          owner = "anwilk";
+          repo = "openpeon-wc2_sapper";
+          tag = "v1.0.0";
+          hash = "sha256-LU/H4EEu2DQIoxt+RNMpgRUC5YfoE5ylPIt53fppXZQ=";
+        };
+      }
+      {
+        name = "wc3_corrupted_arthas";
+        src = pkgs.fetchFromGitHub {
+          owner = "OmegaZero";
+          repo = "openpeon-corrupted-arthas";
+          tag = "v1.0.3";
+          hash = "sha256-604+8bkEek6XUDHDe4Lalxrp3N2SHW/k+baLqbD46Q4=";
+        };
+      }
+      {
+        name = "wc3_jaina";
+        src = pkgs.fetchFromGitHub {
+          owner = "OmegaZero";
+          repo = "openpeon-jaina";
+          tag = "v1.0.0";
+          hash = "sha256-4owVZII7N6fSb+L+dbK0dBt9KavnGGZnWEnG2QeWbmI=";
+        };
+      }
+      {
+        name = "wow-tauren";
+        src = pkgs.fetchFromGitHub {
+          owner = "taylan";
+          repo = "openpeon-wow-tauren";
+          tag = "v1.0.0";
+          hash = "sha256-dIleUfDQJjnbKCAzzYIOYvuAdkXse70O22vdCf1VTv0=";
+        };
+      }
+    ];
   };
 }
