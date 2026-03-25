08b40be875f59301e867848959650fc7c0145b88
Niclas Overby <niclas@overby.me>
2026-03-25 10:31:14 +0100

    feat(rust/systemd): Add --marked flag support for reload-or-restart and set-property Markers
    Signed-off-by: Niclas Overby <niclas@overby.me>

--- rust/systemd/default.nix
+++ rust/systemd/default.nix
@@ -313,8 +313,6 @@
           sed -i '/^# disable --now with template unit/,/^# add-wants\/add-requires/{/^# add-wants\/add-requires/!d}' TEST-26-SYSTEMCTL.sh
           # Remove add-wants/add-requires (not implemented)
           sed -i '/^# add-wants\/add-requires/,/^# set-property/{/^# set-property/!d}' TEST-26-SYSTEMCTL.sh
-          # Remove --marked tests
-          sed -i '/^# --marked/,/^# --dry-run/{/^# --dry-run/!d}' TEST-26-SYSTEMCTL.sh
           # Remove is-system-running (system has degraded state due to boot-time service failures)
           sed -i '/^assert_eq.*is-system-running/d' TEST-26-SYSTEMCTL.sh
           # Remove sysv-generator test
