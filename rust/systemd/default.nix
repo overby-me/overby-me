05c0365506bbd29609abb8be542ef38ddde47ba7
Niclas Overby <niclas@overby.me>
2026-03-30 14:47:41 +0200

    fix(rust/systemd): Use SI units for IO bandwidth limits and add IO property display for services
    Enable TEST-19-CGROUP.keyed-properties.sh by:
    - Adding parse_byte_size_si (base 1000) for IO bandwidth limits matching upstream systemd
    - Adding IO property display to insert_service_config for systemctl show
    - Adding IO property handling in set-property in-memory update for services
    - Fixing multi-line property formatting to repeat key= prefix per line
    
    Signed-off-by: Niclas Overby <niclas@overby.me>

--- rust/systemd/default.nix
+++ rust/systemd/default.nix
@@ -3800,10 +3800,9 @@
       {
         name = "19-CGROUP";
         patchScript = ''
-          # Remove subtests needing DynamicUser, BPF IP filtering, or runtime set-property
+          # Remove subtests needing DynamicUser or BPF IP filtering
           rm -f TEST-19-CGROUP.delegate.sh \
-               TEST-19-CGROUP.IPAddressAllow-Deny.sh \
-               TEST-19-CGROUP.keyed-properties.sh
+               TEST-19-CGROUP.IPAddressAllow-Deny.sh
         '';
       }
       {
