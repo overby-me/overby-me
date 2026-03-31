a1a96a8816a3344a04741abb3ccacc89e12297a3
Niclas Overby <niclas@overby.me>
2026-03-31 21:32:12 +0200

    feat(web/wiki): Read PUBLIC_GIT_COMMIT_SHA from env flake input in Nix build
    Signed-off-by: Niclas Overby <niclas@overby.me>

--- web/wiki/default.nix
+++ web/wiki/default.nix
@@ -6,7 +6,18 @@
     ];
   };
 
-  packages.wiki-frontend = {lib, ...}:
+  packages.wiki-frontend = {
+    lib,
+    inputs,
+    ...
+  }: let
+    envJson = lib.readFile inputs.env.outPath;
+    env =
+      if envJson != ""
+      then lib.fromJSON envJson
+      else {};
+    gitCommitSha = env.PUBLIC_GIT_COMMIT_SHA or "dev";
+  in
     lib.buildDenoProject {
       pname = "wiki-frontend";
       src = lib.fileset.toSource {
@@ -23,6 +34,7 @@
         ];
       };
       buildCommand = ''
+        export PUBLIC_GIT_COMMIT_SHA="${gitCommitSha}"
         deno run -A npm:@gqty/cli generate
         deno run -A npm:@rsbuild/core build
       '';
