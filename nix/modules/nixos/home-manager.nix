a99b427556d7dd22652d0f5d6eaebe7db06eab59
Niclas Overby <niclas@overby.me>
2026-04-24 21:21:24 +0200

    fix(nix): Add backupFileExtension to prevent HM clobber errors on switch
    Signed-off-by: Niclas Overby <niclas@overby.me>

--- nix/modules/nixos/home-manager.nix
+++ nix/modules/nixos/home-manager.nix
@@ -9,6 +9,7 @@
     inherit (inputs.self) users;
     useGlobalPkgs = true;
     useUserPackages = true;
+    backupFileExtension = "hm-backup";
     extraSpecialArgs = {
       inherit inputs pkgs stateVersion src;
     };
