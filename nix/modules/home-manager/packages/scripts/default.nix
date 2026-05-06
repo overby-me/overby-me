e9347372c94e46d21b337261b0b0d14b3cdb486b
Niclas Overby <niclas@overby.me>
2026-05-06 11:51:28 +0200

    chore(nix/modules): Refactor scripts package list using map
    Signed-off-by: Niclas Overby <niclas@overby.me>

--- nix/modules/home-manager/packages/scripts/default.nix
+++ nix/modules/home-manager/packages/scripts/default.nix
@@ -4,25 +4,13 @@
   ...
 }: {
   home = {
-    packages = [
-      (
-        pkgs.writeScriptBin "vi" (lib.readFile ./vi)
-      )
-      (
-        pkgs.writeScriptBin "uf" (lib.readFile ./uf)
-      )
-      (
-        pkgs.writeScriptBin "zed-uf" (lib.readFile ./zed-uf)
-      )
-      (
-        pkgs.writeScriptBin "zellij-cwd" (lib.readFile ./zellij-cwd)
-      )
-      (
-        pkgs.writeScriptBin "nix-flamegraph" (lib.readFile ./nix-flamegraph)
-      )
-      (
-        pkgs.writeScriptBin "git-jj-wrapper" (lib.readFile ./git-jj-wrapper)
-      )
+    packages = map (name: pkgs.writeScriptBin name (lib.readFile ./${name})) [
+      "vi"
+      "uf"
+      "zed-uf"
+      "zellij-cwd"
+      "nix-flamegraph"
+      "git-jj-wrapper"
     ];
   };
 }
