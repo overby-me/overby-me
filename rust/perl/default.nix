e2c94127c6b897fe46b9fe6341f381cea0ae3926
Niclas Overby <niclas@overby.me>
2026-05-07 09:31:50 +0200

    feat(rust/perl): Add 41 more byte-for-byte-passing tests to nix testsuite
    Verified zero-diff against reference perl. Suites added:
    
    t/op/ (17): 64bitint alarm blocks dump groups hexfp int isa
    numify_chkflags override rand readdir readline readline_nb
    smartmatch stat_errors substr_left
    
    t/io/ (11): binmode closepid dup iofile openpid pipe say
    scalar scalar_ungetc socket socketpair
    
    t/mro/ (3): recursion recurse recursion_c3
    
    t/re/ (10): anyof charset no_utf8_pm qrstack regexp_email
    regexp_email_full reg_fold reg_x runtime regex_sets
    
    Bumps the rust-perl nix-checked test count from 135 to 176.
    
    Signed-off-by: Niclas Overby <niclas@overby.me>

--- rust/perl/default.nix
+++ rust/perl/default.nix
@@ -526,6 +526,74 @@
         category = "op";
         name = "wantarray_thr";
       }
+      {
+        category = "op";
+        name = "64bitint";
+      }
+      {
+        category = "op";
+        name = "alarm";
+      }
+      {
+        category = "op";
+        name = "blocks";
+      }
+      {
+        category = "op";
+        name = "dump";
+      }
+      {
+        category = "op";
+        name = "groups";
+      }
+      {
+        category = "op";
+        name = "hexfp";
+      }
+      {
+        category = "op";
+        name = "int";
+      }
+      {
+        category = "op";
+        name = "isa";
+      }
+      {
+        category = "op";
+        name = "numify_chkflags";
+      }
+      {
+        category = "op";
+        name = "override";
+      }
+      {
+        category = "op";
+        name = "rand";
+      }
+      {
+        category = "op";
+        name = "readdir";
+      }
+      {
+        category = "op";
+        name = "readline";
+      }
+      {
+        category = "op";
+        name = "readline_nb";
+      }
+      {
+        category = "op";
+        name = "smartmatch";
+      }
+      {
+        category = "op";
+        name = "stat_errors";
+      }
+      {
+        category = "op";
+        name = "substr_left";
+      }
 
       # --- t/comp/ (selected) ---
       {
@@ -576,8 +644,64 @@
         category = "mro";
         name = "next";
       }
+      {
+        category = "mro";
+        name = "recursion";
+      }
+      {
+        category = "mro";
+        name = "recurse";
+      }
+      {
+        category = "mro";
+        name = "recursion_c3";
+      }
 
       # --- t/io/ (selected) ---
+      {
+        category = "io";
+        name = "binmode";
+      }
+      {
+        category = "io";
+        name = "closepid";
+      }
+      {
+        category = "io";
+        name = "dup";
+      }
+      {
+        category = "io";
+        name = "iofile";
+      }
+      {
+        category = "io";
+        name = "openpid";
+      }
+      {
+        category = "io";
+        name = "pipe";
+      }
+      {
+        category = "io";
+        name = "say";
+      }
+      {
+        category = "io";
+        name = "scalar";
+      }
+      {
+        category = "io";
+        name = "scalar_ungetc";
+      }
+      {
+        category = "io";
+        name = "socket";
+      }
+      {
+        category = "io";
+        name = "socketpair";
+      }
       {
         category = "io";
         name = "argv";
@@ -615,6 +739,46 @@
       {
         category = "re";
         name = "subst";
+      }
+      {
+        category = "re";
+        name = "anyof";
+      }
+      {
+        category = "re";
+        name = "charset";
+      }
+      {
+        category = "re";
+        name = "no_utf8_pm";
+      }
+      {
+        category = "re";
+        name = "qrstack";
+      }
+      {
+        category = "re";
+        name = "regexp_email";
+      }
+      {
+        category = "re";
+        name = "regexp_email_full";
+      }
+      {
+        category = "re";
+        name = "reg_fold";
+      }
+      {
+        category = "re";
+        name = "reg_x";
+      }
+      {
+        category = "re";
+        name = "runtime";
+      }
+      {
+        category = "re";
+        name = "regex_sets";
       }
 
       # --- t/run/ (selected) ---
