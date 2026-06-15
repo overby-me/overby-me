fba08c4bfe4f31db5c532816f3aec2fa5081ca05
Niclas Overby <niclas@overby.me>
2026-06-15 17:39:24 +0200

    chore(nix): Drop pkgs.lib builtins overlay now that lib exposes builtins

--- nix/modules/flakelight/lib.nix
+++ nix/modules/flakelight/lib.nix
@@ -12,11 +12,6 @@
 # Modules that define `perSystemLib` attrs are automatically routed to
 # the perSystemLib option (system-dependent functions taking pkgs).
 # All other attrs are merged into the flake's lib output (system-independent).
-#
-# All builtins are merged into lib at the flake level (see flake.nix) by
-# extending nixpkgs lib before importing flakelight.  This means every
-# flakelight module receives a lib that includes lib.readDir, lib.fromJSON,
-# lib.unsafeDiscardStringContext, etc. without needing builtins.
 {
   config,
   lib,
@@ -63,13 +58,4 @@
 in {
   lib = lib.mkForce mergedLib;
   perSystemLib = mergedPerSystemLib;
-
-  # Extend pkgs.lib with all builtins so that code receiving pkgs.lib
-  # (callPackage arguments, overlays, etc.) also has lib.readDir,
-  # lib.fromJSON, lib.unsafeDiscardStringContext and friends.
-  withOverlays = [
-    (_: prev: {
-      lib = prev.lib.extend (_: prev': builtins // prev');
-    })
-  ];
 }
