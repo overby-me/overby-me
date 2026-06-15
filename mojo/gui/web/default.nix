81ed2f2698d70837d4ef53a8349126fbb216dcef
Niclas Overby <niclas@overby.me>
2026-06-15 14:06:09 +0200

    fix(mojo/wasm): Gate servo behind Linux in mojo-wasm devShell for Darwin

--- mojo/gui/web/default.nix
+++ mojo/gui/web/default.nix
@@ -1,16 +1,22 @@
 {
-  devShells.mojo-wasm = pkgs: {
-    packages = with pkgs; [
-      just
-      mojo
-      deno
-      wabt
-      llvmPackages_latest.llvm
-      llvmPackages_latest.lld
-      wasmtime.lib
-      wasmtime.dev
-      servo
-      jq
-    ];
+  devShells.mojo-wasm = pkgs: let
+    inherit (pkgs) lib stdenv;
+  in {
+    packages = with pkgs;
+      [
+        just
+        mojo
+        deno
+        wabt
+        llvmPackages_latest.llvm
+        llvmPackages_latest.lld
+        wasmtime.lib
+        wasmtime.dev
+        jq
+      ]
+      # Servo browser engine is broken on Darwin in nixpkgs.
+      ++ lib.optionals stdenv.isLinux [
+        servo
+      ];
   };
 }
