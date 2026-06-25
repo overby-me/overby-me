6f3fc3941e9925095dd9701141238da4af79ce06
Niclas Overby <niclas@overby.me>
2026-06-25 14:43:58 +0200

    fix(modules/home-manager): Force overwrite of ssh config file

--- modules/home-manager/programs/ssh.nix
+++ modules/home-manager/programs/ssh.nix
@@ -31,23 +31,31 @@ in {
       };
     };
   };
-  # Needed for ssh to work in buildFHSEnv
-  home.activation = {
-    copySSHConfig = lib.hm.dag.entryAfter ["linkGeneration"] ''
-      configPath="$HOME/.ssh/config"
-      if [ -L "$configPath" ]; then
-        target="$(readlink "$configPath")"
-        run rm "$configPath"
-        run cp "$target" "$configPath"
-        run chmod 600 "$configPath"
-      fi
-    '';
-  };
 
-  home.file = lib.mkIf hasSecrets (let
-    inherit (secrets) publicKeys;
-  in {
-    ".ssh/id_ed25519.pub".text = publicKeys.overby-me-ssh-ed25519;
-    ".ssh/id_rsa.pub".text = publicKeys.overby-me-ssh-rsa;
-  });
+  home = {
+    # Needed for ssh to work in buildFHSEnv and Yocto
+    activation = {
+      copySSHConfig = lib.hm.dag.entryAfter ["linkGeneration"] ''
+        configPath="$HOME/.ssh/config"
+        if [ -L "$configPath" ]; then
+          target="$(readlink "$configPath")"
+          run rm "$configPath"
+          run cp "$target" "$configPath"
+          run chmod 600 "$configPath"
+        fi
+      '';
+    };
+
+    file =
+      {
+        # Forced because copySSHConfig below replaces the symlink with a real copy.
+        ".ssh/config".force = true;
+      }
+      // lib.optionalAttrs hasSecrets (let
+        inherit (secrets) publicKeys;
+      in {
+        ".ssh/id_ed25519.pub".text = publicKeys.overby-me-ssh-ed25519;
+        ".ssh/id_rsa.pub".text = publicKeys.overby-me-ssh-rsa;
+      });
+  };
 }
