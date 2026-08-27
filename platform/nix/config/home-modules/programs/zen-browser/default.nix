a9ddf157a62082c2c7979619cffd1b103d405256
Niclas Overby <niclas@overby.me>
2026-08-27 16:55:23 +0200

    feat(platform/nix/config): Add Nix search engines to Zen
    Signed-off-by: Niclas Overby <niclas@overby.me>

--- platform/nix/config/home-modules/programs/zen-browser/default.nix
+++ platform/nix/config/home-modules/programs/zen-browser/default.nix
@@ -1,10 +1,39 @@
-_: {
+{pkgs, ...}: let
+  nixIcon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+
+  engine = name: alias: template: extra:
+    {
+      inherit name;
+      urls = [{inherit template;}];
+      definedAliases = [alias];
+    }
+    // extra;
+
+  # `force` is what makes any of this stick: Zen rewrites the search config on
+  # every launch otherwise. It also replaces whatever is there, so the engines
+  # worth keeping are declared here too; an empty attrset means the built-in of
+  # that id, which is how Qwant survives being the default.
+  search = {
+    force = true;
+    default = "qwant";
+    engines = {
+      qwant = {};
+      ddg = {};
+      wikipedia = {};
+
+      nixpkgs = engine "Nixpkgs" "!pkgs" "https://search.nixos.org/packages?query={searchTerms}" {icon = nixIcon;};
+      nixos-options = engine "NixOS Options" "!nix" "https://search.nixos.org/options?query={searchTerms}" {icon = nixIcon;};
+      home-manager-options = engine "Home Manager Options" "!hm" "https://home-manager-options.extranix.com/?query={searchTerms}" {icon = nixIcon;};
+    };
+  };
+in {
   programs.zen-browser = {
     enable = true;
     #nativeMessagingHosts = [pkgs.firefoxpwa];
     profiles = rec {
       default = {
         isDefault = true;
+        inherit search;
         settings = {
           "browser.ml.enable" = true;
           "browser.ml.chat.enabled" = true;
